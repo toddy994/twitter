270b7ca123ba58795d1e39b766f9e11767f302ba
Philip Cunningham <philip@irisconnect.co.uk>
2014-06-12 13:03:12 +0100

    extract follower object.

--- twitter.rb
+++ twitter.rb
@@ -1,6 +1,16 @@
 require "pry"
 require "twitter"
 
+class Follower
+  def initialize(client)
+    @client = client
+  end
+
+  def follow(user)
+    @client.follow(user)
+  end
+end
+
 client = Twitter::REST::Client.new do |config|
   config.consumer_key        = ""
   config.consumer_secret     = ""
@@ -8,23 +18,15 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = ""
 end
 
+follower = Follower.new(client)
+
 def search_for_tweets_to(hashtag, client)
   client.search("##{hashtag}", count: 100)
 end
 
-def follow_user(user, client)
-  client.follow(user)
-end
-
-def unfollow_user(user, client)
-  client.unfollow(user)
-end
-
-number_to_follow = ARGV.shift.to_i
-
 ARGV.each do |hashtag|
   search_for_tweets_to(hashtag, client).each do |tweet|
-    follow_user(tweet.user, client)
+    follower.follow(tweet.user)
     sleep 5
   end
 end
